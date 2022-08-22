038a7d35930244400da5b493636267bc3989f486
cvv730 <cvv730@yahoo.com>
2022-08-22 18:21:47 +0100

    FIRST COMMIT

---
+++ src/main/java/com/projexacademy/elearnbackend/ELearnBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.projexacademy.elearnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearnBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELearnBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projexacademy/elearnbackend/ELearnBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projexacademy.elearnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearnBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
